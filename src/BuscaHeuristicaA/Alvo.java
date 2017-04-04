1912df729cf6244bbcd8b4469ffc66d6462d38e6
diegofsousa <diegofernando5672@gmail.com>
2017-04-04 15:51:56 -0300

    graph struct started

---
+++ src/BuscaHeuristicaA/Alvo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BuscaHeuristicaA;
+
+/**
+ *
+ * @author diego
+ */
+public class Alvo {
+    private Vertice alvo;
+
+    public Alvo(Vertice alvo) {
+        this.alvo = alvo;
+    }
+
+    public Vertice getAlvo() {
+        return alvo;
+    }
+
+    public void setAlvo(Vertice alvo) {
+        this.alvo = alvo;
+    }
+    
+}
