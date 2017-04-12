d3ae765c45189a89f7f1a9324efc4f56d31dcec9
diegofsousa <diegofernando5672@gmail.com>
2017-04-12 13:31:27 -0300

    panel "aresta" started

--- src/BuscaHeuristicaA/GUI.java
+++ src/BuscaHeuristicaA/GUI.java
@@ -8,6 +8,7 @@ package BuscaHeuristicaA;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import javax.swing.*;
 
 /**
@@ -16,13 +17,15 @@ import javax.swing.*;
  */
 public class GUI extends JFrame implements ActionListener{
     JTextField campoNovoVertice;
-    JTextField campoNovoVerticeDistancia;
+    JTextField campoNovoVerticeDistancia, campoNovaArestaDist;
     JButton botaoNovoVertice;
-    JPanel listaDeVertices;
-    JList lista;
-    DefaultListModel modelo;
+    JPanel listaDeVertices, listaDeArestas;
+    JList lista, listaAresta;
+    DefaultListModel modelo, modeloAresta;
     String stringAlvo;
     Alvo alvo;
+    ArrayList<Vertice> vertices = new ArrayList<>();
+    JComboBox listaVesticesHome, listaVesticesAway;
 
     public GUI(String stringAlvo) {
         this.stringAlvo = stringAlvo;
@@ -58,6 +61,8 @@ public class GUI extends JFrame implements ActionListener{
         botaoNovoVertice.setSize(160, 25);
         botaoNovoVertice.setLocation(550, 50);
         
+        
+        
         /*Elementos visuais da lista de vertices*/
         
         modelo = new DefaultListModel();
@@ -69,15 +74,13 @@ public class GUI extends JFrame implements ActionListener{
         JScrollPane scrollPane = new JScrollPane();
 
         //modelo.add(0, "diego fernando");
-        
         scrollPane.getViewport().add(lista);
-
+        
         listaDeVertices = new JPanel();
         listaDeVertices.setLayout(new GridLayout(0, 2));
         listaDeVertices.setSize(1500,200);
         listaDeVertices.setLocation(30, 105);
         listaDeVertices.add(scrollPane);
-        
         
         painelVertices.add(labelNovoVertice);
         painelVertices.add(labelNovoVerticeNome);
@@ -86,6 +89,81 @@ public class GUI extends JFrame implements ActionListener{
         painelVertices.add(campoNovoVerticeDistancia);
         painelVertices.add(botaoNovoVertice);
         painelVertices.add(listaDeVertices);
+        
+        /* Elementos visuais de painelArestas */
+        JLabel labelNovaAresta = new JLabel("Adicione uma nova aresta:");
+        labelNovaAresta.setSize(200, 25);
+        labelNovaAresta.setLocation(20, 20);
+        
+        JLabel labelNovoArestaHome = new JLabel("Partida:");
+        labelNovoArestaHome.setSize(100, 25);
+        labelNovoArestaHome.setLocation(20, 50);
+        
+        listaVesticesHome = new JComboBox();
+        listaVesticesHome.setSize(100, 25);
+        listaVesticesHome.setLocation(90, 50);
+        
+        //campoNovoVertice = new JTextField();
+        //campoNovoVertice.setSize(200, 25);
+        //campoNovoVertice.setLocation(80, 50);
+        
+        JLabel labelNovoArestaAway = new JLabel("Chegada:");
+        labelNovoArestaAway.setSize(100, 25);
+        labelNovoArestaAway.setLocation(220, 50);
+        
+        listaVesticesAway = new JComboBox();
+        listaVesticesAway.setSize(100, 25);
+        listaVesticesAway.setLocation(300, 50);
+        
+        //campoNovoVerticeDistancia = new JTextField();
+        //campoNovoVerticeDistancia.setSize(80, 25);
+        //campoNovoVerticeDistancia.setLocation(440, 50);
+        
+        JLabel labelNovaArestaDist = new JLabel("Dist.:");
+        labelNovaArestaDist.setSize(100, 25);
+        labelNovaArestaDist.setLocation(430, 50);
+        
+        campoNovaArestaDist = new JTextField();
+        campoNovaArestaDist.setSize(80, 25);
+        campoNovaArestaDist.setLocation(500, 50);
+        
+        JButton botaoNovaAresta = new JButton("Adicionar aresta");
+        botaoNovaAresta.setSize(160, 25);
+        botaoNovaAresta.setLocation(600, 50);
+        
+        modeloAresta = new DefaultListModel();
+        listaAresta = new JList(modeloAresta);
+        listaAresta.setSize(700, 200);
+        listaAresta.setLocation(50, 65);
+        
+        
+        JScrollPane scrollPaneAresta = new JScrollPane();
+
+        //modelo.add(0, "diego fernando");
+        
+        
+        
+        scrollPaneAresta.getViewport().add(listaAresta);
+
+        listaDeArestas = new JPanel();
+        listaDeArestas.setLayout(new GridLayout(0, 2));
+        listaDeArestas.setSize(1500,200);
+        listaDeArestas.setLocation(30, 105);
+        listaDeArestas.add(scrollPaneAresta);
+        
+        painelArestas.add(labelNovaAresta);
+        painelArestas.add(labelNovoArestaHome);
+        painelArestas.add(botaoNovaAresta);
+        painelArestas.add(labelNovoArestaAway);
+        painelArestas.add(listaDeArestas);
+        painelArestas.add(listaVesticesHome);
+        painelArestas.add(listaVesticesAway);
+        painelArestas.add(labelNovaArestaDist);
+        painelArestas.add(campoNovaArestaDist);
+        painelArestas.setLayout(null);
+        
+        
+        
         painelVertices.setLayout(null);
         
         botaoNovoVertice.addActionListener(this);
@@ -114,9 +192,9 @@ public class GUI extends JFrame implements ActionListener{
          if(e.getActionCommand().equals("Adicionar vértice")){
              System.out.println("Adicionando vértice");
              if(validarCamposVertices(campoNovoVertice.getText(), campoNovoVerticeDistancia.getText())){
+                 vertices.add(new Vertice(campoNovoVertice.getText(), Double.parseDouble(campoNovoVerticeDistancia.getText())));
                  modelo.add(0, "Vértice: "+ campoNovoVertice.getText() + " - Distância para o alvo: " + campoNovoVerticeDistancia.getText());
-             }
-             
+             } 
          }
     }
     
